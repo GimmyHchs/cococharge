d9bb0a05447b10ad2971b3129f30ed01789a357a
Hchs <g9308370@hotmail.com>
2018-05-27 14:08:00 +0800

    feat(migration): add legacy migrations

---
+++ database/migrations/2017_03_06_160000_create_wallets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name')->default('My Wallets');
+            $table->double('origin_amount')->default(0);
+            $table->double('balance')->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
