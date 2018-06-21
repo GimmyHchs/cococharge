098383a22038d4204ac37137b90f84af2ac5c118
Hchs <g9308370@hotmail.com>
2018-06-21 22:07:23 +0800

    test(action): improve coverage rate

--- tests/Unit/Services/SyntaxGate/Actions/InitialLineAcctionTest.php
+++ tests/Unit/Services/SyntaxGate/Actions/InitialLineAcctionTest.php
@@ -12,15 +12,31 @@ class InitialLineAccountTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testExecute()
+    public function testExecuteWillCreateNewAccount()
     {
         $text = factory(LineText::class)->states('withMessageEvent')->create();
         $event = $text->messageEvent;
+
         $action = app(InitialLineAccount::class);
         $action->execute($event);
 
         $this->assertEquals(1, LineAccount::all()->count());
         $this->assertEquals($event->source_type, LineAccount::first()->type);
         $this->assertEquals($event->source_id, LineAccount::first()->line_id);
+    }
+
+    public function testExecuteWontCreateNewAccount()
+    {
+        $text = factory(LineText::class)->states('withMessageEvent')->create();
+        $event = $text->messageEvent;
+        factory(LineAccount::class)->create([
+            'line_id' => $event->getSourceId(),
+            'type' => $event->getSourceType(),
+        ]);
+
+        $action = app(InitialLineAccount::class);
+        $action->execute($event);
+
+        $this->assertEquals(1, LineAccount::all()->count());
     }
 }
